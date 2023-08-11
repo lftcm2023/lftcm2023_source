8119163906652a9c58bde13df4feb04d9eaaba02
Jon Eugster <eugster.jon@gmail.com>
2023-08-11 13:17:18 +0200

    add Projects folder

--- lakefile.lean
+++ lakefile.lean
@@ -10,4 +10,9 @@ lean_lib MIL {
   -- add library configuration options here
 }
 
+@[default_target]
+lean_lib Projects {
+  -- add library configuration options here
+}
+
 require mathlib from git "https://github.com/leanprover-community/mathlib4"@"master"
