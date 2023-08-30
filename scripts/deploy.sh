6dc1633517cf2092961dcf2a788a0e4259d04268
Alexander Bentkamp <bentkamp@gmail.com>
2023-08-30 10:21:47 +0200

    deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 set -e
-if [ "$#" -ne 2 ]; then
-    echo "Usage example: $0 leanprover-community mathematics_in_lean"
-    exit 1
-fi
+# if [ "$#" -ne 2 ]; then
+#     echo "Usage example: $0 leanprover-community mathematics_in_lean"
+#     exit 1
+# fi
 
 # Build
 scripts/mkall.py
-make clean html latexpdf
-cp -Lr build/html user_repo/
-cp build/latex/mathematics_in_lean.pdf user_repo/
+cp -r .vscode user_repo/.vscode
+# make clean html latexpdf
+# cp -Lr build/html user_repo/
+# cp build/latex/mathematics_in_lean.pdf user_repo/
 
 # Deploy
 rm -rf deploy
-git clone git@github.com:$1/$2 deploy
+git clone git@github.com:lftcm2023/lftcm2023 deploy
 cd deploy
 rm -rf * .gitignore
 cp -Lr ../user_repo/./ .
@@ -22,14 +23,14 @@ git add -f .vscode
 git commit -m "Update `date`"
 git push
 
-git checkout gh-pages
-rm -rf * .gitignore .buildinfo .nojekyll
-cp -r ../build/html/./ .
-cp ../build/latex/mathematics_in_lean.pdf .
-touch .nojekyll
-git add .
-git commit -m "Update `date`"
-git push
+# git checkout gh-pages
+# rm -rf * .gitignore .buildinfo .nojekyll
+# cp -r ../build/html/./ .
+# cp ../build/latex/mathematics_in_lean.pdf .
+# touch .nojekyll
+# git add .
+# git commit -m "Update `date`"
+# git push
 
 cd ..
 rm -rf deploy
